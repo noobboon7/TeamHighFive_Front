94c6f2ba6a9c31139e0b4d4420a59380743a1aba
berryny <judesire@yahoo.com>
2020-06-14 14:19:39 -0400

    installed react-countup for counter animation and fixed styles

--- src/components/home/Connections.js
+++ src/components/home/Connections.js
@@ -6,6 +6,7 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
+import CountUp from 'react-countup';
 
 const Connections = () => (
     <div className='connectionDiv container-fluid d-flex flex-column align-items-center py-5 my-5'>
@@ -22,14 +23,14 @@ const Connections = () => (
               <span className="counter-icon">
                 <FontAwesomeIcon icon={faCalendarAlt} className="m-0" />
               </span>
-              <p className="counter-number">17</p>
+              <p className="counter-number"><CountUp end={17} duration={5} /></p>
               <p className="counter-label">Summer Programs</p>
             </div>
             <div className="col-md-3 text-center">
               <span className="counter-icon">
                 <FontAwesomeIcon icon={faHandshake} className="m-0" />
               </span>
-              <p className="counter-number">17</p>
+              <p className="counter-number"><CountUp end={50} duration={5} /></p>
               <p className="counter-label">Internships</p>
             </div>
 
@@ -37,14 +38,14 @@ const Connections = () => (
               <span className="counter-icon">
                 <FontAwesomeIcon icon={faStar} className="m-0" />
               </span>
-              <p className="counter-number">17</p>
+              <p className="counter-number"><CountUp end={25} duration={5} /></p>
               <p className="counter-label">Top Category<span className="category">Sports</span></p>
             </div>
             <div className="col-md-3 text-center">
               <span className="counter-icon">
                 <FontAwesomeIcon icon={faHeart} className="m-0" />
               </span>
-              <p className="counter-number">17</p>
+              <p className="counter-number"><CountUp end={100} duration={5} /></p>
               <p className="counter-label">Youth Organization</p>
             </div>
           </div>
