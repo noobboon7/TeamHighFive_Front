662a0c531019ee8ba68860994b194f59d78abf0c
Jerry V <jvr572@gmail.com>
2020-06-14 11:28:38 -0400

    searchBar in-progress

--- src/components/organizations/OrgCard.js
+++ src/components/organizations/OrgCard.js
@@ -1,53 +1,28 @@
 import React from "react";
-import {
-  Col,
-  Card
-} from "react-bootstrap";
-import styles from "./OrgCard.module.css";
-import {
-  Link
-} from 'react-router-dom'
+import {Col,Card} from "react-bootstrap";
+// import styles from "./OrgCard.module.css";
+// import {Link} from 'react-router-dom'
 
-const OrgCard = ({
-  organization
-}) => {
+const OrgCard = ({organization}) => {
   function formatDescription() {
-    const newString = organization.description.split(" ").slice(0, 10);
-    return newString.join(" ");
+    let newString = organization.description.split(" ").slice(0, 10);
+    newString = newString.join(" ");
+    return newString + "...";
   }
 
-  return ( <
-    Col className="mb-5" lg = {
-      4
-    }
-    sm = {
-      6
-    } >
-    <
-    Card className="border-0">
-    <
-    Card.Img className = "d-none"
-    src = {
-      organization.logo
-    }
-    /> <
-    Card.Body >
-    <
-    Card.Title > {
-      organization.name
-    } < /Card.Title> <
-    Card.Text > {
-      formatDescription()
-    } < /Card.Text>
-    <
-    Card.Link className="mx-0" href = {
-      organization.url
-    } > {
-      organization.url
-    } < /Card.Link> < /
-    Card.Body > <
-    /Card> < /
-    Col >
+  return ( 
+    <Col className="mb-5" lg = {4}sm = {6} >
+      <Card className="border-0">
+        <Card.Img className = "d-none"src = {organization.logo}/> 
+        <Card.Body >
+          <Card.Title> {organization.name} </Card.Title> 
+          <Card.Text > {formatDescription()} </Card.Text>
+          <Card.Link className="mx-0" href = {organization.url}> 
+            {organization.url} 
+          </Card.Link> 
+        </Card.Body > 
+      </Card> 
+    </Col>
   );
 };
 
--- src/components/organizations/OrganizationList.js
+++ src/components/organizations/OrganizationList.js
@@ -1,18 +1,15 @@
 import React from "react";
 import OrgCard from "./OrgCard";
 
-const OrganizationList = ({
-  organizations_array
-}) => ( <
-  div className="container mt-5" ><div className="row"> {
-    organizations_array.map((organization) => ( <
-      OrgCard organization = {
-        organization
-      }
-      />
-    ))
-  } <
-  /div></div>
+const OrganizationList = ({organizations_array}) => ( 
+  <div className="container mt-5" >
+    <div className="row"> 
+      {organizations_array.map((organization, idx) => ( 
+        <OrgCard key={idx} organization = {organization}/>
+        ))
+      } 
+    </div>
+  </div>
 );
 
 export default OrganizationList;
