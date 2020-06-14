0a6887b2a3e52de2858ea72430e2653a8bcf7a5f
Katharine Francis <katie-francis@hotmail.com>
2020-06-14 16:27:14 -0400

    Presentable ShowPage

--- src/components/pages/ShowPage.js
+++ src/components/pages/ShowPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Banner from "../banner/Banner";
+import { Row, Col } from "react-bootstrap";
 
 const ShowPage = (props) => {
   const [organization, setOrganization] = useState({});
@@ -19,11 +20,47 @@ const ShowPage = (props) => {
       let data = await org.json();
       setOrganization(data);
     }
-  }, [organization]);
+    fetchOrganization();
+  }, []);
 
+  let programs = organization.programs;
+  let lis = programs ? (
+    programs.map((program) => (
+      <Col>
+        <div
+          key={program.id}
+          // style={{
+          //   flexDirection: "column",
+          //   flexWrap: "wrap",
+          //   backgroundColor: "grey",
+          //   height: "16em",
+          //   width: "18em",
+          //   color: "white",
+          //   justifyContent: "center",
+          //   textAlign: "center",
+          //   padding: "5px",
+          //   margin: "5px",
+          // }}
+        >
+          <h3>{program.name}</h3>
+        </div>
+      </Col>
+    ))
+  ) : (
+    <></>
+  );
   return (
     <div>
       <Banner name={organization.name} />
+      <div className="container mt-5">
+        <Row>
+          <h1>{organization.name}</h1>
+          <p>{organization.description}</p>
+        </Row>
+      </div>
+      {/* <Row>
+        <ul>{lis}</ul>
+      </Row> */}
     </div>
   );
 };
