6467a106ba00618c218dc49d354835d14d9ddb53
berryny <judesire@yahoo.com>
2020-06-14 13:46:44 -0400

    update styles for counter container

--- src/components/home/Connections.js
+++ src/components/home/Connections.js
@@ -1,17 +1,57 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+import { faHandshake } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 
 const Connections = () => (
-    <div className='connectionDiv'>
+    <div className='connectionDiv container-fluid d-flex flex-column align-items-center py-5 my-5'>
         <div className='connect-head-div'>
-            <p className='connect-head' > PREPARED FOR SUCCESS</p>
+            <span className='intro-lead'>Prepare for Success</span>
         </div>
-        <div className='connect-title-div'>
+        <div className='text-white'>
             <h1>We're Pround of our Connections</h1>
         </div>
-        
-        <button className='connectBtn'>Join the Movement</button>                  
+
+        <div className="connection-counter-wrapper container text-white my-3">
+          <div className="row">
+            <div className="col-md-3 text-center">
+              <span className="counter-icon">
+                <FontAwesomeIcon icon={faCalendarAlt} className="m-0" />
+              </span>
+              <p className="counter-number">17</p>
+              <p className="counter-label">Summer Programs</p>
+            </div>
+            <div className="col-md-3 text-center">
+              <span className="counter-icon">
+                <FontAwesomeIcon icon={faHandshake} className="m-0" />
+              </span>
+              <p className="counter-number">17</p>
+              <p className="counter-label">Internships</p>
+            </div>
+
+            <div className="col-md-3 text-center">
+              <span className="counter-icon">
+                <FontAwesomeIcon icon={faStar} className="m-0" />
+              </span>
+              <p className="counter-number">17</p>
+              <p className="counter-label">Top Category<span className="category">Sports</span></p>
+            </div>
+            <div className="col-md-3 text-center">
+              <span className="counter-icon">
+                <FontAwesomeIcon icon={faHeart} className="m-0" />
+              </span>
+              <p className="counter-number">17</p>
+              <p className="counter-label">Youth Organization</p>
+            </div>
+          </div>
+        </div>
+
+        <button className='connectBtn connectBtnSolid'>Join the Movement</button>
     </div>
   );
-  
+
   export default Connections;
