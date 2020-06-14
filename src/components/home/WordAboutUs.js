4cef62f3e974f9f31a73647724ff51198804b849
berryny <judesire@yahoo.com>
2020-06-14 12:04:17 -0400

    update WordAboutUs styles and placement on page

--- src/components/home/WordAboutUs.js
+++ src/components/home/WordAboutUs.js
@@ -2,20 +2,22 @@ import React from "react";
 import teamDreamers from "../../static/teamDreamers.png";
 
 const WordAboutUs = () => (
-    <div className='wordDiv'>
-        <div className='textDiv'>
-            <p className='head' > A Word About Us</p>
+    <div className='container-fluid bg-white p-5 mt-5'>
+      <div className="row align-items-center">
+        <div className='col-lg-6'>
+            <p className='intro-lead m-0'>A Word About Us</p>
             <h1>Connecting Youth For a Better Tomorrow</h1>
             <p className='wordDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis eleifend ante et gravida. Duis quam ligula, consectetur ac justo at, feugiat condimentum lectus. Quisque congue rutrum lorem egestas tincidunt. Nunc vitae dapibus diam. </p>
             <div className='buttonDiv'>
-                <button className='wordBtn'>Add a Program</button>        
-                <button className='wordBtn'>Find Programs</button>        
+                <a href="#" className='wordBtn wordBtnSolid'>Add a Program</a>
+                <a href="#" className='wordBtn wordBtnOutline'>Find Programs</a>
             </div>
-    </div>
-        <div className='imageDiv'>
-            <img src={teamDreamers}></img>
         </div>
+        <div className='col-lg-6'>
+            <img src={teamDreamers} className="d-block mx-auto img-fluid"></img>
+        </div>
+      </div>
     </div>
   );
-  
+
   export default WordAboutUs;
