cf4b3d98cf9a837ed9d561c00e141589398c8797
Jerry V <jvr572@gmail.com>
2020-06-14 16:33:31 -0400

    redirecting to Results

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 import Hero from "../hero/Hero";
 import OrganizationList from "../organizations/OrganizationList";
 import WordAboutUs from "../home/WordAboutUs";
-// import Connections from "../home/Connections";
+import Connections from "../home/Connections";
 
 const Home = ({ programArray }) => (
   <div>
