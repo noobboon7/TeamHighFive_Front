9a05729833b0c4305c7e1d021e5be4f967c0da07
parkerbacall <Pbacall2@gmail.com>
2020-06-14 10:45:28 -0600

    connection component

---
+++ src/components/home/Connections.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Connections = () => (
+    <div className='connectionDiv'>
+        <div className='connect-head-div'>
+            <p className='connect-head' > PREPARED FOR SUCCESS</p>
+        </div>
+        <div className='connect-title-div'>
+            <h1>We're Pround of our Connections</h1>
+        </div>
+        
+        <button className='connectBtn'>Join the Movement</button>                  
+    </div>
+  );
+  
+  export default Connections;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,13 @@ import React from "react";
 import Hero from "../hero/Hero";
 import OrganizationList from "../organizations/OrganizationList";
 import WordAboutUs from "../home/WordAboutUs";
+import Connections from "../home/Connections";
 
 const Home = ({ organizations_array }) => (
   <div>
     <Hero organizations_array={organizations_array} />
     <WordAboutUs/>
+    <Connections/>
     <OrganizationList organizations_array={organizations_array} />
   </div>
 );
