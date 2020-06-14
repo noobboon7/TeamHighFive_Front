f4346a4a6d9b6ba19f3dadb13ea9cd1b2c2d65b7
berryny <judesire@yahoo.com>
2020-06-13 22:45:45 -0400

    bootstrap nav changes to navbar

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,7 +3,7 @@ import styles from "./Hero.module.css";
 import SearchBars from "../searchbars/SearchBars";
 
 const Hero = () => (
-  <div className={styles.heroStyles}>
+  <div className="d-flex justify-content-center">
     <SearchBars />
   </div>
 );
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -5,32 +5,34 @@ import Logo from "../static/youth_connection_logo.jpg";
 
 const Layout = ({ children }) => (
   <div>
-    <Navbar collapseOnSelect expand="lg" sticky="top">
+    <Navbar collapseOnSelect light expand="md" className="custom-navbar" sticky="top">
       <Navbar.Brand href="#home">
-        <img src={Logo}></img>
+        <img className="img-fluid logo" src={Logo}></img>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="justify-content-center">
+      <Navbar.Collapse id="responsive-navbar-nav" className="d-flex justify-content-around">
+        <Nav>
           <Nav.Link href="#">HOME</Nav.Link>
           <Nav.Link href="#">ABOUT</Nav.Link>
           <Nav.Link href="#">HOW IT WORKS</Nav.Link>
           <Nav.Link href="#">ADD AN ORGANIZATION</Nav.Link>
           <Nav.Link href="#">CONTACT</Nav.Link>
         </Nav>
-        <Nav className="justify-content-end">
+        <Nav>
           <Nav.Link href="#">ORGANIZATION LOGIN</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
     {children}
     <Footer>
-      <Row>
-        <div>&copy; High-Five</div>
-      </Row>
-      <Row>
-        <div>About</div>
-      </Row>
+{
+      // <Row>
+      //   <div>&copy; High-Five</div>
+      // </Row>
+      // <Row>
+      //   <div>About</div>
+      // </Row>
+}
     </Footer>
   </div>
 );
