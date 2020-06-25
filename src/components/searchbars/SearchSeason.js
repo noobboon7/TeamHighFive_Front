73e8b19e122142e81318673c410f1404e8e21c3e
berryny <judesire@yahoo.com>
2020-06-25 16:16:41 -0400

    add styling css and components for searchbar layout

--- src/components/searchbars/SearchSeason.js
+++ src/components/searchbars/SearchSeason.js
@@ -1,32 +1,36 @@
 import React, {useState} from "react";
 import {
+	Col,
 	InputGroup,
   Dropdown,
   DropdownButton,
+	ButtonGroup
 } from "react-bootstrap";
 
 const SearchSeason = ({getVal,valObj}) => {
   const seasons = ['Summer', 'Spring', 'Fall', 'Winter', 'Year-round'];
   const [season, setSeason] = useState('Season');
 	return (
-		<InputGroup>
+		<Col md="3" className="p-0 searchbar-wrapper">
 			<DropdownButton
 				value='season'
 				onSelect={(e) => {
 					getVal(valObj => ({...valObj, season:e}));
 					setSeason(e);
 				}}
-				as={InputGroup.Append}
-				variant='info'
+				as={ButtonGroup}
+				variant='light'
 				title={season}
-				id='input-group-dropdown-2'>
+				id='input-group-dropdown-2'
+				className="w-100"
+				>
 				{seasons.map((program, idx) => (
 					<Dropdown.Item eventKey={program} key={idx}>
 						{program}
 					</Dropdown.Item>
 				))}
 			</DropdownButton>
-		</InputGroup>
+		</Col>
 	);
 };
 
