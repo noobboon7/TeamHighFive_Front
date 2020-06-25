9be15b79a8b8ef83bc9f02dd41b9a042d135e000
Katharine Francis <katie-francis@hotmail.com>
2020-06-25 16:35:59 -0400

    A little shjuj

--- src/components/searchbars/SearchCategory.js
+++ src/components/searchbars/SearchCategory.js
@@ -1,35 +1,34 @@
-import React, {useState} from "react";
-import {
-  InputGroup,
-  Dropdown,
-  DropdownButton,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import { InputGroup, Dropdown, DropdownButton } from "react-bootstrap";
 
-const SearchCategory = ({prgArr, getVal}) => {
-	const [categoryChoice, setCategoryChoice] = useState('Categories');
-	const parseCurrentAvaliableCategories = () => {
-		let categoriesArr = prgArr.map(program => program.categories);
-		categoriesArr = categoriesArr.flat();
-		categoriesArr = new Set(categoriesArr);
-		return Array.from(categoriesArr);
-	};
-	return(
-		<InputGroup>
-			<DropdownButton
-				onSelect={(e) => {
-					getVal(valObj => ({...valObj, category: e}));
-					setCategoryChoice(e)
-				}}
-				as={InputGroup.Append}
-				variant='info'
-				title={categoryChoice}
-				id='input-group-dropdown-2'>
-				{parseCurrentAvaliableCategories().map((program, idx) => (
-					<Dropdown.Item eventKey={program} key={idx}>{program}</Dropdown.Item>
-					))}
-			</DropdownButton>
-		</InputGroup>
-	)
+const SearchCategory = ({ prgArr, getVal }) => {
+  const [categoryChoice, setCategoryChoice] = useState("Categories");
+  const parseCurrentAvaliableCategories = () => {
+    let categoriesArr = prgArr.map((program) => program.categories);
+    categoriesArr = categoriesArr.flat();
+    categoriesArr = new Set(categoriesArr);
+    return Array.from(categoriesArr);
+  };
+  return (
+    <InputGroup>
+      <DropdownButton
+        onSelect={(e) => {
+          getVal((valObj) => ({ ...valObj, category: e }));
+          setCategoryChoice(e);
+        }}
+        as={InputGroup.Append}
+        variant="info"
+        title={categoryChoice}
+        id="input-group-dropdown-2"
+      >
+        {parseCurrentAvaliableCategories().map((program, idx) => (
+          <Dropdown.Item eventKey={program} key={idx}>
+            {program}
+          </Dropdown.Item>
+        ))}
+      </DropdownButton>
+    </InputGroup>
+  );
 };
 
 export default SearchCategory;
