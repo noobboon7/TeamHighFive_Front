3edb922a73f4db948058f658c897e50369437b30
berryny <judesire@yahoo.com>
2020-06-29 12:54:21 -0400

    fix css homepage layout

--- src/components/home/HowItWorks.js
+++ src/components/home/HowItWorks.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const HowItWorks = () => (
     <div className='container-fluid bg-white p-5 mt-5 how-it-works'>
-      <div className="row align-items-center justify-content-end">
+      <div className="row align-items-center justify-content-start">
         <div className='col-lg-6 how-it-works-body p-5'>
             <p className='intro-lead m-0'>How it Works</p>
             <h1>Connecting Youth For a Better Tomorrow</h1>
-            <p className='wordDescription'>Every member of the Youth Connection team cares deeply about the racial injustices that affect people of color everday. We believe that we can be a force for change by empowering under privilaged youth to connect with non-profits that cater specifically to them. </p>
+            <p className='wordDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis eleifend ante et gravida. Duis quam ligula, consectetur ac justo at, feugiat condimentum lectus. Quisque congue rutrum lorem egestas tincidunt. Nunc vitae dapibus diam. </p>
             <div className='buttonDiv text-center'>
                 <a href="https://forms.gle/8UpNNNfrWh5Exnvd9" target="_blank" rel="noopener noreferrer" className='wordBtn wordBtnSolid'>Add a Program</a>
                 <a href="/" className='wordBtn wordBtnOutline'>Find Programs</a>
