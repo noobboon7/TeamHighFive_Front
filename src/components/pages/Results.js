64fdf8de5e685b94cd9eabe46618d39b18f0cf27
berryny <judesire@yahoo.com>
2020-07-06 17:49:32 -0400

    form search template

--- src/components/pages/Results.js
+++ src/components/pages/Results.js
@@ -4,7 +4,7 @@ import useFilterResults from "../../hooks/useFilterResults";
 import SearchLocation from "../searchbars/SearchLocation";
 import ProgramsList from "../programsList";
 
-import { Row, Col} from "react-bootstrap";
+import { Row, Col, Form} from "react-bootstrap";
 
 
 const Results = ({ allPrograms, homeFilterArr }) => {
@@ -17,12 +17,42 @@ const Results = ({ allPrograms, homeFilterArr }) => {
 
   return (
 		<>
-      <Row className='w-50 align-content-start flex-column mx-auto'>
-        <Col md='5' className='p-0'>
-          <h6 className='text-black mt-3'>Filter by</h6>
-        </Col>
-        <SearchLocation prgArr={allPrograms} getVal={setValue}/>
-      </Row>
+      {
+			// 	<Row className='w-50 align-content-start flex-column mx-auto'>
+      //   <Col md='5' className='p-0'>
+      //     <h6 className='text-black mt-3'>Filter by</h6>
+      //   </Col>
+      //   <SearchLocation prgArr={allPrograms} getVal={setValue}/>
+      // </Row>
+		}
+
+			<Form className="px-5">
+			<h6 className='text-black mt-3'>Filter by</h6>
+			  <Form.Row>
+					<Form.Group as={Col} md="4">
+						<Form.Label for="byCategories">Category</Form.Label>
+						<Form.Control as="select" id="byCategories">
+							<option>sports</option>
+							<option>technologyk</option>
+						</Form.Control>
+					</Form.Group>
+			    <Form.Group as={Col} md="4">
+						<Form.Label for="byLocation">Location</Form.Label>
+			      <Form.Control as="select" id="byLocation">
+							<option>Brooklyn, New York</option>
+							<option>New York, New York</option>
+						</Form.Control>
+			    </Form.Group>
+
+			    <Form.Group as={Col} md="4">
+						<Form.Label for="bySeason">Season</Form.Label>
+						<Form.Control as="select" id="bySeason">
+							<option>Spring</option>
+							<option>Summer</option>
+						</Form.Control>
+			    </Form.Group>
+			  </Form.Row>
+			</Form>
 
 			<ProgramsList prgArr={allPrograms} filteredPrograms={(filteredResults||homeFilterChoice)} />
 		</>
