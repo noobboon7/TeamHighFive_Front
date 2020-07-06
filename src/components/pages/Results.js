65ad1a5a7725a93a280cdba50b60a4838750f025
Jerry V <jvr572@gmail.com>
2020-07-06 18:13:44 -0400

    moved to component

--- src/components/pages/Results.js
+++ src/components/pages/Results.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import useFilterResults from "../../hooks/useFilterResults";
+import ResultsPageSearch from '../searchbars/ResultsPageSearch'
 
-import SearchLocation from "../searchbars/SearchLocation";
+// import SearchLocation from "../searchbars/SearchLocation";
 import ProgramsList from "../programsList";
 
-import { Row, Col, Form} from "react-bootstrap";
+// import { Badge, Button} from "react-bootstrap";
 
 
 const Results = ({ allPrograms, homeFilterArr }) => {
@@ -13,48 +14,23 @@ const Results = ({ allPrograms, homeFilterArr }) => {
   let homeFilterChoice = JSON.parse(sessionStorage.getItem('userSelects2'));
 	const filteredResults = useFilterResults(allPrograms, value);
 
-  console.log(('results',filteredResults||'home:',homeFilterChoice))
+  // console.log(('results',filteredResults||'home:',homeFilterChoice))
 
   return (
 		<>
-      {
-			// 	<Row className='w-50 align-content-start flex-column mx-auto'>
-      //   <Col md='5' className='p-0'>
-      //     <h6 className='text-black mt-3'>Filter by</h6>
-      //   </Col>
-      //   <SearchLocation prgArr={allPrograms} getVal={setValue}/>
-      // </Row>
-		}
+			{/* <Button variant='primary'>
+				Profile <Badge variant='light'>9</Badge>
+				<span className='sr-only'>unread messages</span>
+			</Button> */}
+			{
+				//   <SearchLocation prgArr={allPrograms} getVal={setValue}/>
+			}
 
-			<Form className="px-5">
-			<h6 className='text-black mt-3'>Filter by</h6>
-			  <Form.Row>
-					<Form.Group as={Col} md="4">
-						<Form.Label for="byCategories">Category</Form.Label>
-						<Form.Control as="select" id="byCategories">
-							<option>sports</option>
-							<option>technologyk</option>
-						</Form.Control>
-					</Form.Group>
-			    <Form.Group as={Col} md="4">
-						<Form.Label for="byLocation">Location</Form.Label>
-			      <Form.Control as="select" id="byLocation">
-							<option>Brooklyn, New York</option>
-							<option>New York, New York</option>
-						</Form.Control>
-			    </Form.Group>
-
-			    <Form.Group as={Col} md="4">
-						<Form.Label for="bySeason">Season</Form.Label>
-						<Form.Control as="select" id="bySeason">
-							<option>Spring</option>
-							<option>Summer</option>
-						</Form.Control>
-			    </Form.Group>
-			  </Form.Row>
-			</Form>
-
-			<ProgramsList prgArr={allPrograms} filteredPrograms={(filteredResults||homeFilterChoice)} />
+			<ResultsPageSearch />
+			<ProgramsList
+				prgArr={allPrograms}
+				filteredPrograms={filteredResults || homeFilterChoice}
+			/>
 		</>
 	);
 };
---
+++ src/components/searchbars/ResultsPageSearch.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Col, Form} from "react-bootstrap";
+
+const ResultsPageSearchBar = () => (
+
+  <Form className="px-5">
+			<h6 className='text-black mt-3'>Filter by</h6>
+			  <Form.Row>
+					<Form.Group as={Col} md="4">
+						<Form.Label for="byCategories">Category</Form.Label>
+						<Form.Control as="select" id="byCategories">
+							<option>sports</option>
+							<option>technologyk</option>
+						</Form.Control>
+					</Form.Group>
+			    <Form.Group as={Col} md="4">
+						<Form.Label for="byLocation">Location</Form.Label>
+			      <Form.Control as="select" id="byLocation">
+							<option>Brooklyn, New York</option>
+							<option>New York, New York</option>
+						</Form.Control>
+			    </Form.Group>
+
+			    <Form.Group as={Col} md="4">
+						<Form.Label for="bySeason">Season</Form.Label>
+						<Form.Control as="select" id="bySeason">
+							<option>Spring</option>
+							<option>Summer</option>
+						</Form.Control>
+			    </Form.Group>
+			  </Form.Row>
+			</Form>
+)
+
+export default ResultsPageSearchBar;
