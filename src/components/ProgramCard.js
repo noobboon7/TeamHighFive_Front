7ac288677a4f929e3638aacf527ea7710a27f747
Jerry V <jvr572@gmail.com>
2020-07-06 20:05:50 -0400

    minor ixes

--- src/components/ProgramCard.js
+++ src/components/ProgramCard.js
@@ -5,13 +5,16 @@ import { Link } from "react-router-dom";
 
 //add color "info" to button and height
 const ProgramCard = ({ program }) => {
-	console.log('program',program);
+	// console.log('program',program);
 	function formatDescription() {
 		let newString = program.description.split(" ").slice(0, 10);
 		newString = newString.join(" ");
 		return newString + "...";
 	}
-
+	const catBadges = () => {
+		return program.categories.map(cat => <Badge variant='info'>{cat}</Badge>);
+	}
+		
 	return (
 		<Col
 			className='mb-5 col-lg-4 d-flex align-items-stretch organizationList-col'
@@ -20,18 +23,23 @@ const ProgramCard = ({ program }) => {
 			<Card className='border-0'>
 				<Card.Img className='d-none' src={program.logo} />
 				<Card.Body>
-					<Card.Text className="intro-lead m-0">{program.start_season}</Card.Text>
+					<Card.Text className='intro-lead m-0'>
+						{program.start_season === "N/A" ? 
+							"Year-Round"
+							: 
+							program.start_season}
+					</Card.Text>
 					<Card.Title>{program.name}</Card.Title>
 					<Card.Text>{formatDescription()}</Card.Text>
 					<Card.Text>{program.location}</Card.Text>
-					<Card.Text><Badge variant="info">{program.categories}</Badge></Card.Text>
+					<Card.Text>{catBadges()}</Card.Text>
 					<Card.Link className='mx-0' href={program.url}>
 						{program.url}
 					</Card.Link>
 				</Card.Body>
 				<Card.Footer className='text-center'>
 					<Link to={`/organization/${program.organization_id}`}>
-						<Button variant='info' renderas='button'>
+						<Button variant='info' renders='button'>
 							<span>Learn More</span>
 						</Button>
 					</Link>
